39df51ca920920927e630d24ca3582da348421d0
linas <linas@0e1ec626-d7d3-11df-80ee-83e85c865f91>
2010-11-08 20:23:09 +0000

    First few templates.

--- siom/views.py
+++ siom/views.py
@@ -5,4 +5,4 @@ from siom.models import Course
 
 def course_index(request):
 	course_list = get_list_or_404(Course)
-	return render_to_response('course_index.html', {'courses': course_list})
+	return render_to_response('course_index.html', {'course_list': course_list})

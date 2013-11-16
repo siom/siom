342d8905327498078e6129c07e203b7777c9b8f1
donatas <donatas@0e1ec626-d7d3-11df-80ee-83e85c865f91>
2013-11-16 11:32:42 +0000

    Scoreboard.

--- siom/templatetags/stats.py
+++ siom/templatetags/stats.py
@@ -1,7 +1,7 @@
 from django import template
 from django.template.loader import render_to_string
 
-from siom.models import Submission
+from siom.models import Submission, Entry
 
 register = template.Library()
 
@@ -35,3 +35,36 @@ def results_table(context, tasks):
 		{'tasks': tasks, 'submissions': submission_table},
 		context)
 
+@register.simple_tag(takes_context=True)
+def scoreboard_table(context, course):
+	user_subs = {}
+	for submission in Submission.objects.filter(user__courses=course):
+		if submission.user not in user_subs:
+			user_subs[submission.user] = []
+		user_subs[submission.user].append(submission)
+	
+	#TODO: move to models?
+	tasks = set()
+	for entry in Entry.objects.filter(courses=course):
+		for task in entry.tasks.all():
+			tasks.add(task)
+		#tasks = tasks | set(entry.tasks)
+
+	scores = []
+	for user in user_subs:
+		score = {}
+		score['user'] = user
+		solved = set()
+		for sub in user_subs[user]:
+			if sub.verdict:
+				solved.add(sub.task)
+				#solved[sub.task] = True
+		solved = filter(lambda x : x in tasks, solved)
+		score['score'] = len(solved)
+		scores.append(score)
+	scores.sort(reverse=True, key=lambda score:score['score'])
+
+	return render_to_string('tags/scoreboard_table.html', {
+		'scores': scores,
+		'total': len(tasks),
+	}, context)

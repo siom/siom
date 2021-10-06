06e6635d8755689de2dc788b0409c3ac88d2110e
Vytautas Strimaitis <vstrimaitis@gmail.com>
2021-10-06 15:49:20 +0000

    Download static files on startup instead of keeping them in source control

--- .devcontainer/scripts/init_filesystem.sh
+++ .devcontainer/scripts/init_filesystem.sh
@@ -3,5 +3,4 @@ set -e
 
 mkdir -p "$SIOM_HOME/logs"
 mkdir -p "$SIOM_HOME/grader/tasks"
-ln -s /workspace "$SIOM_HOME/web" # f?
-ln -s /workspace/static "$SIOM_HOME/static" # f?
+ln -s /workspace "$SIOM_HOME/web"
--- .devcontainer/scripts/post_start.sh
+++ .devcontainer/scripts/post_start.sh
@@ -4,12 +4,15 @@ set -e
 echo "Setting up database"
 echo "... Waiting for database to spin up"
 sleep 5
-echo "... Ensuring correct encoding"
+echo "... Ensuring correct database encoding"
 mysql -h db -P 3306 -u root --password=root -e 'ALTER DATABASE siom CHARACTER SET utf8'
 echo "... Applying migrations"
 python manage.py migrate
 echo "... Seeding data"
 python manage.py loaddata .devcontainer/seed_data/db.json
+
+echo "Collecting static files"
+python manage.py collectstatic --no-input
 
 echo "Starting celery worker"
 nohup python manage.py celery worker -l info -c 1 </dev/null &> /home/www/siom/logs/celery.txt &
@@ -17,4 +20,5 @@ nohup python manage.py celery worker -l info -c 1 </dev/null &> /home/www/siom/l
 echo "Starting web server"
 nohup python manage.py runserver </dev/null &> /home/www/siom/logs/server.txt &
 
-sleep 5
+echo "Creating required folders"
+mkdir -p /home/www/siom/web/latex_tmp
